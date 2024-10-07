2fd6a63c3b18652050fb9c66fdd56cc58d08ee31
svip7 <14146420+svip7@user.noreply.gitee.com>
2024-10-07 11:59:18 +0800

    Site updated: 2024-10-07 11:59:17

--- js/fomal.js
+++ js/fomal.js
@@ -3178,10 +3178,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(https://tuchuang.voooe.cn/images/2024/08/24/B56C447A7AB4B4F0EA222FB1EE3CE212-01.jpg);
+    --darkmode-bg:url(https://tuchuang.voooe.cn/images/2024/08/24/B56C447A7AB4B4F0EA222FB1EE3CE212-01.jpg);
+    --mobileday-bg: url(https://tuchuang.voooe.cn/images/2024/08/24/B56C447A7AB4B4F0EA222FB1EE3CE212-01.jpg);
+    --mobilenight-bg: url(https://tuchuang.voooe.cn/images/2024/08/24/B56C447A7AB4B4F0EA222FB1EE3CE212-01.jpg);
   }`;
 }
 // 切换背景主函数
